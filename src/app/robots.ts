8f9535e2cf9923fcb0e492c84fabd0a8f17d0946
RemasteredGod <97588514+RemasteredGod@users.noreply.github.com>
2025-10-11 19:48:38 +0000

    updated robot.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://ginni-interior.com';
+  const baseUrl = 'https://www.ginniinterior.com/';
 
   return {
     rules: [
